a9c6a810f66f5ccf801fd65dd5571c4832fafa70
Valik Grinevich <valik.v1per@gmail.com>
2014-11-19 15:01:32 +0200

    Add entity

--- src/Valiknet/Blog/PostsBundle/Entity/Comment.php
+++ src/Valiknet/Blog/PostsBundle/Entity/Comment.php
@@ -12,17 +12,17 @@ class Comment {
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
-     * @ORM\GenerateValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
-     * @ORM\Column(type="string", length="200")
+     * @ORM\Column(type="string", length=200)
      */
     protected $author;
 
     /**
-     * @ORM\Column(type="string", length="255")
+     * @ORM\Column(type="string", length=255)
      */
     protected $text;
 
@@ -32,8 +32,110 @@ class Comment {
     protected $create_at;
 
     /**
-     * @ORM\ManyToOne(TargetEntity="Post", inversedBy="comment")
+     * @ORM\ManyToOne(targetEntity="Post", inversedBy="comment")
      * @ORM\JoinColumn(name="post_id", referencedColumnName="id")
      */
     protected $post;
-} 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Comment
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Comment
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set create_at
+     *
+     * @param \DateTime $createAt
+     * @return Comment
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->create_at = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get create_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreateAt()
+    {
+        return $this->create_at;
+    }
+
+    /**
+     * Set post
+     *
+     * @param \Valiknet\Blog\PostsBundle\Entity\Post $post
+     * @return Comment
+     */
+    public function setPost(\Valiknet\Blog\PostsBundle\Entity\Post $post = null)
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    /**
+     * Get post
+     *
+     * @return \Valiknet\Blog\PostsBundle\Entity\Post 
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+}
--- src/Valiknet/Blog/PostsBundle/Entity/Post.php
+++ src/Valiknet/Blog/PostsBundle/Entity/Post.php
@@ -12,12 +12,12 @@ class Post {
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
-     * @ORM\GenerateValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
-     * @ORM\Column(type="string", length="150")
+     * @ORM\Column(type="string", length=150)
      */
     protected $name;
 
@@ -45,4 +45,180 @@ class Post {
      * @ORM\OneToMany(targetEntity="Tag", mappedBy="post")
      */
     protected $tag;
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->comment = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->tag = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Post
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Post
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set create_at
+     *
+     * @param \DateTime $createAt
+     * @return Post
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->create_at = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get create_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreateAt()
+    {
+        return $this->create_at;
+    }
+
+    /**
+     * Set update_at
+     *
+     * @param \DateTime $updateAt
+     * @return Post
+     */
+    public function setUpdateAt($updateAt)
+    {
+        $this->update_at = $updateAt;
+
+        return $this;
+    }
+
+    /**
+     * Get update_at
+     *
+     * @return \DateTime 
+     */
+    public function getUpdateAt()
+    {
+        return $this->update_at;
+    }
+
+    /**
+     * Add comment
+     *
+     * @param \Valiknet\Blog\PostsBundle\Entity\Comment $comment
+     * @return Post
+     */
+    public function addComment(\Valiknet\Blog\PostsBundle\Entity\Comment $comment)
+    {
+        $this->comment[] = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Remove comment
+     *
+     * @param \Valiknet\Blog\PostsBundle\Entity\Comment $comment
+     */
+    public function removeComment(\Valiknet\Blog\PostsBundle\Entity\Comment $comment)
+    {
+        $this->comment->removeElement($comment);
+    }
+
+    /**
+     * Get comment
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Add tag
+     *
+     * @param \Valiknet\Blog\PostsBundle\Entity\Tag $tag
+     * @return Post
+     */
+    public function addTag(\Valiknet\Blog\PostsBundle\Entity\Tag $tag)
+    {
+        $this->tag[] = $tag;
+
+        return $this;
+    }
+
+    /**
+     * Remove tag
+     *
+     * @param \Valiknet\Blog\PostsBundle\Entity\Tag $tag
+     */
+    public function removeTag(\Valiknet\Blog\PostsBundle\Entity\Tag $tag)
+    {
+        $this->tag->removeElement($tag);
+    }
+
+    /**
+     * Get tag
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
 }
