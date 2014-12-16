b46d96af05d7c9bb71c153540ddd37f51ad0aa6f
Valik Grinevich <valik.v1per@gmail.com>
2014-12-17 00:54:10 +0200

    add some tests

--- src/Valiknet/Blog/PostsBundle/Tests/Services/CommentHandlerTest.php
+++ src/Valiknet/Blog/PostsBundle/Tests/Services/CommentHandlerTest.php
@@ -13,16 +13,7 @@ class CommentHandlerTest extends WebTestCase
 
         $post = $container->get('doctrine')
             ->getRepository('ValiknetBlogPostsBundle:Post')
-            ->findBySlugPost('nope');
-
-        $result = $container->get('valiknet.blog.postsbundle.services.comment_handler')
-            ->handleAddComment($post);
-
-        $this->assertInstanceOf('NotFoundHttpException', $result);
-
-        $post = $container->get('doctrine')
-            ->getRepository('ValiknetBlogPostsBundle:Post')
-            ->findBySlugPost('lorem-ipsum-1521');
+            ->findOneBySlugPost('lorem-ipsum-1521');
 
         $result = $container->get('valiknet.blog.postsbundle.services.comment_handler')->handleAddComment($post);
 
