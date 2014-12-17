ee3ad91d508e32c86f49ef1b9be03b71953d502d
Valik Grinevich <valik.v1per@gmail.com>
2014-12-17 15:58:48 +0200

    add insert all added category in update post

--- src/Valiknet/Blog/PostsBundle/Form/DataTransformer/StringToArrayTransformer.php
+++ src/Valiknet/Blog/PostsBundle/Form/DataTransformer/StringToArrayTransformer.php
@@ -1,18 +1,43 @@
 <?php
 namespace Valiknet\Blog\PostsBundle\Form\DataTransformer;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Form\DataTransformerInterface;
 
 class StringToArrayTransformer implements DataTransformerInterface
 {
+    private $em;
+    private $data;
 
-    public function transform($string)
+    public function __construct(EntityManager $em = null, $data = null)
     {
+        if ($em !== null) {
+            $this->em = $em;
+            $this->data = $data;
+        }
+    }
+
+    public function transform($array)
+    {
+        if ($array === null) {
+            return "";
+        }
+
+        $post = $this->em->getRepository('ValiknetBlogPostsBundle:Post')
+                ->findOneById($this->data->getId());
+
+        $tags = [];
+
+        foreach ($post->getTag() as $tag) {
+            $tags[] = $tag->getHashTag();
+        }
+
+        return implode(", " , $tags);
     }
 
     public function reverseTransform($string)
     {
-        if (!$string) {
+        if ($string === null) {
             return array();
         }
 
