cdbd53dbd4ed4c550de8982415e69f00254c3d09
Valik Grinevich <valik.v1per@gmail.com>
2015-02-07 10:11:25 +0200

    make updates in controller

--- src/Valiknet/Blog/PostsBundle/Controller/UserController.php
+++ src/Valiknet/Blog/PostsBundle/Controller/UserController.php
@@ -77,6 +77,7 @@ class UserController extends BaseController
             // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
             $error = $error->getMessage();
         }
+
         // last username entered by the user
         $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
 
