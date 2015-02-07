f6eeab48857bab156bad25d5ce1b4109bfe9ea32
Valik Grinevich <valik.v1per@gmail.com>
2015-02-07 21:49:19 +0200

    update configs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Valiknet\BlogBundle\ValiknetBlogPostsBundle(),
+            new Valiknet\BlogBundle\ValiknetBlogBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
