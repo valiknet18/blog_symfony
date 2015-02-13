013e708a7b7e75e2cdfe3eff408a11eb391b902b
Valik Grinevich <valik.v1per@gmail.com>
2015-02-13 02:18:55 +0200

    update user document

--- src/Valiknet/UserBundle/Document/User.php
+++ src/Valiknet/UserBundle/Document/User.php
@@ -10,9 +10,24 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 class User extends BaseUser
 {
     /**
-     * @ODM\Id(strategy="auto")
+     * @ODM\Id()
      */
     protected $id;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $firstName;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    protected $lastName;
+
+    /**
+     * @ODM\ReferenceMany(targetDocument="Valiknet\BlogBundle\Document\Post")
+     */
+    protected $posts;
 
     public function __construct()
     {
@@ -27,5 +42,79 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Add post
+     *
+     * @param Valiknet\BlogBundle\Document\Post $post
+     */
+    public function addPost(\Valiknet\BlogBundle\Document\Post $post)
+    {
+        $this->posts[] = $post;
+    }
+
+    /**
+     * Remove post
+     *
+     * @param Valiknet\BlogBundle\Document\Post $post
+     */
+    public function removePost(\Valiknet\BlogBundle\Document\Post $post)
+    {
+        $this->posts->removeElement($post);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return Doctrine\Common\Collections\Collection $posts
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return self
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string $firstName
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return self
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string $lastName
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
     }
 }
